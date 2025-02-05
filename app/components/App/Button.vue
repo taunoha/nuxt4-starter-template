fb05b38f8d7eb2ab70fd0e08f61d4fa6abf4509f
Tauno Hanni <taunoh@gmail.com>
2025-02-05 12:22:16 +0200

    feat: tailwind-merge

--- app/components/App/Button.vue
+++ app/components/App/Button.vue
@@ -1,7 +1,15 @@
 <script lang="ts">
+import { tv } from "tailwind-variants";
+import { twMerge } from "tailwind-merge";
+
+const button = tv({
+  base: "bg-brand-500 hover:bg-brand-100 hover:text-brand-500 ring-brand-500 inline-flex items-center gap-2 rounded-lg px-5 py-2 font-medium text-white inset-ring inset-shadow-sm ring shadow-md inset-shadow-white/20 inset-ring-white/15 transition-colors duration-150",
+});
+
 interface Props {
   type?: "button" | "submit" | "reset";
   to?: string;
+  class?: string;
 }
 </script>
 <script setup lang="ts">
@@ -12,24 +20,17 @@ defineOptions({
 const props = withDefaults(defineProps<Props>(), {
   type: "button",
   to: undefined,
+  class: "",
 });
 const attrs = useAttrs();
+
+const ui = computed(() => twMerge(button(), props.class));
 </script>
 <template>
-  <button
-    v-if="!props.to"
-    :type="props.type"
-    v-bind="attrs"
-    class="bg-brand-500 hover:bg-brand-100 hover:text-brand-500 ring-brand-500 inline-flex items-center gap-2 rounded-lg px-5 py-2 font-medium text-white inset-ring inset-shadow-sm ring shadow-md inset-shadow-white/20 inset-ring-white/15 transition-colors duration-150"
-  >
+  <button v-if="!props.to" :type="props.type" v-bind="attrs" :class="ui">
     <slot />
   </button>
-  <NuxtLink
-    v-else
-    :to="props.to"
-    v-bind="attrs"
-    class="bg-brand-500 hover:bg-brand-100 hover:text-brand-500 ring-brand-500 inline-flex items-center gap-2 rounded-lg px-5 py-2 font-medium text-white inset-ring inset-shadow-sm ring shadow-md inset-shadow-white/20 inset-ring-white/15 transition-colors duration-150"
-  >
+  <NuxtLink v-else :to="props.to" v-bind="attrs" :class="ui">
     <slot />
   </NuxtLink>
 </template>
