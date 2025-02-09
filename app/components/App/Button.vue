0094a871f3a0adf5463b6a5ea94d07e3097f8400
Tauno Hanni <taunoh@gmail.com>
2025-02-09 12:01:47 +0200

    feat: Update AppButton component

--- app/components/App/Button.vue
+++ app/components/App/Button.vue
@@ -3,13 +3,26 @@ import { tv } from "tailwind-variants";
 import { twMerge } from "tailwind-merge";
 
 const button = tv({
-  base: "bg-brand-500 hover:bg-brand-100 hover:text-brand-500 ring-brand-500 inline-flex items-center gap-2 rounded-lg px-5 py-2 font-medium text-white inset-ring inset-shadow-sm ring shadow-md inset-shadow-white/20 inset-ring-white/15 transition-colors duration-150",
+  base: "inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg px-8 py-3 font-medium transition-colors duration-150",
+  variants: {
+    variant: {
+      outline:
+        "text-brand border-brand-500 hover:not-disabled:bg-brand-100 hover:not-disabled:text-brand-500 border bg-white",
+      solid:
+        "bg-brand-500 box-reflect-below hover:not-disabled:bg-brand-100 hover:not-disabled:text-brand-500 ring-brand-500 shadow-brand-700/40 text-white inset-ring inset-shadow-sm ring shadow-md inset-shadow-white/20 inset-ring-white/15",
+    },
+    disabled: {
+      true: "cursor-not-allowed opacity-50 grayscale-25",
+    },
+  },
 });
 
 interface Props {
   type?: "button" | "submit" | "reset";
   to?: string;
   class?: string;
+  disabled?: boolean;
+  variant?: "outline" | "solid";
 }
 </script>
 <script setup lang="ts">
@@ -21,16 +34,35 @@ const props = withDefaults(defineProps<Props>(), {
   type: "button",
   to: undefined,
   class: "",
+  variant: "solid",
+  disabled: false,
 });
 const attrs = useAttrs();
 
-const ui = computed(() => twMerge(button(), props.class));
+const ui = computed(() =>
+  twMerge(
+    button({ variant: props.variant, disabled: props.disabled }),
+    props.class
+  )
+);
 </script>
 <template>
-  <button v-if="!props.to" :type="props.type" v-bind="attrs" :class="ui">
+  <button
+    v-if="!props.to"
+    :type="props.type"
+    :disabled="props.disabled"
+    v-bind="attrs"
+    :class="ui"
+  >
     <slot />
   </button>
-  <NuxtLink v-else :to="props.to" v-bind="attrs" :class="ui">
+  <NuxtLink
+    v-else
+    :to="props.to"
+    v-bind="attrs"
+    :disabled="props.disabled"
+    :class="ui"
+  >
     <slot />
   </NuxtLink>
 </template>
